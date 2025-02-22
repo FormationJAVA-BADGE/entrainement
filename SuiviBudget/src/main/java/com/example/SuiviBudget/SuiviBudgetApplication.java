f72ecb27a285b51986b4e54616dc200d50e911ae
sebPhilippot <philippot.sebastien@gmail.com>
2025-02-22 15:46:44 +0100

    initial commit

---
+++ SuiviBudget/src/main/java/com/example/SuiviBudget/SuiviBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.example.SuiviBudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuiviBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuiviBudgetApplication.class, args);
+	}
+
+}
